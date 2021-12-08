dde204c342cc98ef6d56e3f0b99fd097d04867f7
Habibulla Daniel <user.habibulla@gmail.com>
2021-12-08 23:46:30 +0100

    removed label[i] = obj

--- interview_refactoring.py
+++ interview_refactoring.py
@@ -5,10 +5,8 @@
 # The code filters and separates box labels with x0, y0, x1, y1 coordinates and a class type
 def main(labels):    
     # parse
-
     res2 = []
     res3 = []
-
     for i in range(len(labels)):
         obj = labels[i].split(",")
         for j in range(len(obj)):
@@ -16,13 +14,12 @@ def main(labels):
                 obj[j] = int(obj[j])
             else:
                 obj[j] = float(obj[j])
-        labels[i] = obj
     # filter small labels
-        if not abs(labels[i][0]-labels[i][2]) < 2 and not abs(labels[i][1]-labels[i][3]) < 12:
-            res2.append(labels[i])
+        if not abs(obj[0]-obj[2]) < 2 and not abs(obj[1]-obj[3]) < 12:
+            res2.append(obj)
     # filter pedestrians
-        if labels[i][4] == 5:
-            res3.append(labels[i])
+        if obj[4] == 5:
+            res3.append(obj)
     return res2, res3
 res = main(["0,1,2.2,3,5", "0, 0, 21, 42, 5"])
 print(res)
