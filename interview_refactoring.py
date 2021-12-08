f248082d9910b6d7642c8a0330ce97f4d9e4e892
Habibulla Daniel <user.habibulla@gmail.com>
2021-12-08 23:36:23 +0100

    Removed obj2, third for cycle

--- interview_refactoring.py
+++ interview_refactoring.py
@@ -7,21 +7,20 @@ def main(labels):
     # parse
     for i in range(len(labels)):
         obj = labels[i].split(",")
-        obj2 = []
-        for o in obj[:-1]:
-            obj2.append(float(o))
-        obj2.append(int(obj[4]))
-        labels[i] = obj2
-    #print("OBJ2", obj2)
+        for j in range(len(obj)):
+            if j == 4:
+                obj[j] = int(obj[j])
+            else:
+                obj[j] = float(obj[j])
+        labels[i] = obj
     res = labels
     # filter small labels
     res2 = []
+    res3 = []
     for i in range(len(res)):
         if not abs(res[i][0]-res[i][2]) < 2 and not abs(res[i][1]-res[i][3]) < 12:
             res2.append(res[i])
     # filter pedestrians
-    res3 = []
-    for i in range(len(res)):
         if res[i][4] == 5:
             res3.append(res[i])
     return res2, res3
